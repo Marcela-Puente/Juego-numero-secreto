8a6a6832b552f7b5c46576bcab0d4433aec6e700
Marcela-Puente <fpuentemarcela@gmail.com>
2024-08-27 16:19:04 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,129 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+console.log(numeroSecreto);
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        //El usuario no acertó
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p', 'El número secreto es menor');
+
+        } else {
+            asignarTextoElemento('p', 'El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja() {
+    let valorCaja = document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random() * numeroMaximo) + 1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //Si ya sorteamos todos los números 
+    if (listaNumerosSorteados.length == numeroMaximo) {
+asignarTextoElemento('p','Ya se sortearon todos los números posibles');
+    } else {
+
+        //Si el número generado está incluido en la lista
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento("h1", "Juego del número secreto");
+    asignarTextoElemento("p", `Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego() {
+    //Limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalos de números 
+    //Generar el número aleatorio
+    //Inicializar el número de intentos
+    condicionesIniciales();
+    //Deshabilitar el botón de Nuevo Juego
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+
+}
+
+condicionesIniciales();
+
+
+
+
+
+
+
+
+/*
+function saludo() {
+    console.log("Hola, mundo!")
+}
+
+saludo();
+
+
+
+function mostrarNombre(nombre) {
+    console.log(`¡Hola, ${nombre}!`);
+}
+
+mostrarNombre("Marcela");
+
+
+
+
+function calcularDoble(numero) {
+return numero*2;
+}
+
+let resultadoDoble = calcularDoble();
+console.log(resultadoDoble);
+
+
+
+
+function Calcularpromedio(a, b, c) {
+    return numeros(a + b + c) / 3;
+}
+
+let promedio = promedio(); 
+console.log(promedio);
+*/
+
+
+
+
+
+
+
+
+
